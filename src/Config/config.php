b517c2dc7c88ab74d80f8cc4f48e8e46b478c590
winni <winnienkwinika@gmail.com>
2023-09-14 16:39:16 +0200

    created config file

---
+++ src/Config/config.php
@@ -0,0 +1,12 @@
+<?php
+$numbers_length = 4;
+$letters_length = 2;
+$numbers = substr(str_shuffle('0123456789'),1,$numbers_length);
+$letters = substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ'),1,$letters_length);
+
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_HOST','localhost');
+define('DB_NAME','employee_db');
+define('EMPLOYEE_ID', $letters.$numbers);
+define('ADMIN', 'WN1234');
