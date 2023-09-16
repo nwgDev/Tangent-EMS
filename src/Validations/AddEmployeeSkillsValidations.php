c1d2f0270521a99b6b4a569eab22ccad87188921
winni <winnienkwinika@gmail.com>
2023-09-16 11:15:12 +0200

    created class to validate employee skills

---
+++ src/Validations/AddEmployeeSkillsValidations.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace TANGENT\Validations;
+
+
+
+class AddEmployeeSkillsValidations
+{
+    private $objEmployeeSkiils;
+    private $errors = [];
+
+    public function __construct($employee_skills)
+    {
+        $this->objEmployeeSkiils = $employee_skills;
+    }
+
+    public function validateEmployeeSkills()
+    {
+        $this->validateSkills();
+        return empty($this->errors);
+    }
+
+    private function validateSkills()
+    {
+        $skills = $this->objEmployeeSkiils->getSkills();
+
+        if(!empty($skills) && is_array($skills)) {
+            $this->errors[] = "Skill/s is required.";
+        }
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+}
