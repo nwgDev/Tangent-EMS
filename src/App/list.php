f2d79382ce9c0dfdd1a77d9e572f2777ae75a170
winni <winnienkwinika@gmail.com>
2023-09-20 07:31:25 +0200

    added index to display list of employees

--- src/App/list.php
+++ src/App/list.php
@@ -1,10 +1,34 @@
 <?php
-include_once '../Helpers/EmployeeManager.php';
-include_once '../Config/DBManager.php';
+include_once '../../Helpers/EmployeeManager.php';
+include_once '../../Config/DBManager.php';
 
 use TANGENT\Helpers\EmployeeManager;
 
-$results = EmployeeManager::getAllEmployees();
+$employees = EmployeeManager::getAllEmployees();
 
-header('Content-Type: application/json');
-echo json_encode($results);
+if($employees > 0)
+{
+    foreach($employees as $employee)
+    {
+        ?>
+        <tr>
+            <td><?= $employee['first_name']; ?></td>
+            <td><?= $employee['last_name']; ?></td>
+            <td><?= $employee['contact_number']; ?></td>
+            <td><?= $employee['email_address']; ?></td>
+            <td><?= $employee['country']; ?></td>
+            <td>
+                <a href="view.php?id=<?= $employee['id']; ?>" class="btn btn-info btn-sm">View</a>
+                <a href="edit.php?id=<?= $employee['id']; ?>" class="btn btn-success btn-sm">Edit</a>
+                <form action="../../App/delete.php" method="POST" class="d-inline">
+                    <button type="submit" name="delete_employee" value="<?=$employee['id'];?>" class="btn btn-danger btn-sm">Delete</button>
+                </form>
+            </td>
+        </tr>
+        <?php
+    }
+}
+else
+{
+    echo "<h5> No Record Found </h5>";
+}
---
+++ src/resources/templates/index.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Employees</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<div class="container mt-5">
+    <?php include('message.php'); ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Employees
+                        <a href="add.php" class="btn btn-primary float-end">New Employee</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Contact Number</th>
+                            <th>Email Address</th>
+                            <th>Country</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        include ('../../App/list.php');
+                        ?>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
